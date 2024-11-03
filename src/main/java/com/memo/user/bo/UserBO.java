1e8204ca2c84fd75532248c9b47fe72cc56f3a03
1J-Choi <chldnjswp110@naver.com>
2024-11-03 13:29:47 +0900

    2024 11 01

--- src/main/java/com/memo/user/bo/UserBO.java
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -2,7 +2,9 @@ package com.memo.user.bo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.memo.common.EncryptUtills;
 import com.memo.user.entity.UserEntity;
 import com.memo.user.repository.UserRepository;
 
@@ -16,5 +18,26 @@ public class UserBO {
 	// output: UserEntity
 	public UserEntity getUserEntityByLoginId(String loginId) {
 		return userRepository.findByLoginId(loginId);
+	}
+	
+	// input: 파라미터 4개
+	// output: UserEntity
+	public UserEntity addUser(String loginId, String password, String name, String email) {
+		return userRepository.save(
+				UserEntity.builder()
+				.loginId(loginId)
+				.password(password)
+				.name(name)
+				.email(email)
+				.build());
+	}
+	
+	// input: 파라미터 2개
+	// output: UserEntity
+	public UserEntity getUser(String loginId, String password) {
+		// 비밀번호 해싱
+		String hashedPassword = EncryptUtills.md5(password);
+		
+		return userRepository.findByLoginIdAndPassword(loginId, hashedPassword);
 	}
 }
