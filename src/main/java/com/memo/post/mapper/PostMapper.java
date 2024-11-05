af756e3c2a9ca7ebdb3a90865c6a547fa533c2d1
1J-Choi <chldnjswp110@naver.com>
2024-11-05 12:17:31 +0900

    2024 11 04

--- src/main/java/com/memo/post/mapper/PostMapper.java
+++ src/main/java/com/memo/post/mapper/PostMapper.java
@@ -4,9 +4,14 @@ import java.util.*;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.memo.post.domain.Post;
+
 @Mapper
 public interface PostMapper {
 	// input: X
 	// output: List<Map<String, Object>>
 	public List<Map<String, Object>> selectPostList();
+	// input: userId
+	// output: List<Post>
+	public List<Post> selectPostListByUserId(int userId);
 }
